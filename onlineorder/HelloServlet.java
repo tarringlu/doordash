c12ee7d8e50934a13cbe2e7792f8dabad2059c25
tarringlu <89592975+tarringlu@users.noreply.github.com>
2022-01-23 14:59:06 -0500

    Add files via upload

---
+++ onlineorder/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.laioffer.onlineorder;
+import org.json.JSONObject;
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import org.apache.commons.io.IOUtils;
+import com.laioffer.onlineorder.entity.Customer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ onlineorder/service/ItemOrderService.java
@@ -0,0 +1,40 @@
+package com.laioffer.onlineorder.service;
+import com.laioffer.onlineorder.dao.ItemOrderDao;
+import com.laioffer.onlineorder.entity.Customer;
+import com.laioffer.onlineorder.entity.MenuItem;
+import com.laioffer.onlineorder.entity.OrderItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemOrderService {
+
+    @Autowired
+    private ItemOrderDao itemOrderDao;
+
+    @Autowired
+    private MenuInfoService menuInfoService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    public void saveItem(int menuId) {
+        OrderItem orderItem = new OrderItem();
+        MenuItem menuItem = menuInfoService.getMenuItem(menuId);
+
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String email = loggedInUser.getName();
+
+        Customer customer = customerService.getCustomer(email);
+
+        orderItem.setMenuItem(menuItem);
+        orderItem.setCart(customer.getCart());
+        orderItem.setQuantity(1);
+        orderItem.setPrice(menuItem.getPrice());
+
+        itemOrderDao.save(orderItem);
+    }
+
+}
